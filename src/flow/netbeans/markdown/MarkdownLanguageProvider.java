44bc600ae63b6e4f18bf5a360d4e5529e0786728
madflow <>
2013-03-16 12:00:51 +0100

    Make use of MarkdownLanguageConfig constants

--- src/flow/netbeans/markdown/MarkdownLanguageProvider.java
+++ src/flow/netbeans/markdown/MarkdownLanguageProvider.java
@@ -1,19 +1,21 @@
 package flow.netbeans.markdown;
 
+import flow.netbeans.markdown.csl.MarkdownLanguageConfig;
+import flow.netbeans.markdown.highlighter.MarkdownLanguageHierarchy;
+import flow.netbeans.markdown.highlighter.MarkdownTokenId;
 import org.netbeans.api.lexer.InputAttributes;
 import org.netbeans.api.lexer.Language;
 import org.netbeans.api.lexer.LanguagePath;
 import org.netbeans.api.lexer.Token;
 import org.netbeans.spi.lexer.LanguageEmbedding;
 import org.netbeans.spi.lexer.LanguageProvider;
-import flow.netbeans.markdown.highlighter.MarkdownLanguageHierarchy;
-import flow.netbeans.markdown.highlighter.MarkdownTokenId;
 
 @org.openide.util.lookup.ServiceProvider(service = org.netbeans.spi.lexer.LanguageProvider.class)
 public class MarkdownLanguageProvider extends LanguageProvider {
 
+    @Override
     public Language<MarkdownTokenId> findLanguage(String mimeType) {
-        if ("text/x-markdown".equals(mimeType)) {
+        if (MarkdownLanguageConfig.MIME_TYPE.equals(mimeType)) {
             return new MarkdownLanguageHierarchy().language();
         }
         return null;
