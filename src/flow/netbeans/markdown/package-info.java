dcb07fccd5e9badec288031571e9e91ed94861db
Holger Stenger <holger.stenger@gmx.de>
2017-03-24 23:02:55 +0100

    Replace template registration with annotation

---
+++ src/flow/netbeans/markdown/package-info.java
@@ -0,0 +1,8 @@
+@org.netbeans.api.templates.TemplateRegistration(
+        content = "resources/MarkdownTemplate.md",
+        displayName = "flow.netbeans.markdown.resources.Bundle#Templates/Other/MarkdownTemplate.md",
+        description = "resources/MarkdownTemplateDescription.html",
+        folder = "Other",
+        requireProject = false
+)
+package flow.netbeans.markdown;
